1d66fae4d66d9ab0a4fddf695c899a34cecb691e
7Spook7 <yassinl@vt.edu>
2024-02-03 18:34:30 -0500

    idk aksjdksadank

--- myapp/src/lib/components/Title.svelte
+++ myapp/src/lib/components/Title.svelte
@@ -1,7 +1,3 @@
-<style class="	height: 1px">
-
-
-</style>
 
 <script>
     import { Heading, P, Button } from 'flowbite-svelte';
@@ -9,11 +5,7 @@
   </script>
   
   <div class="text-center">
-    <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
-    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
-    <Button href="/">
-      Learn more
-      <ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
-    </Button>
-  </div>
+    <Heading tag="h1" class="p-8" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Her Housing</Heading>
+    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">We can help you find some great housing 🔥🔥🔥</P>
+  </div>  
 
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -6,6 +6,7 @@
   }
   import SearchBar from '$lib/components/SearchBar.svelte';
   import Title from '$lib/components/Title.svelte';
+  import Properties from '$lib/components/AvailableProperties.svelte';
 
   import { Heading, P } from 'flowbite-svelte';
   import { ArrowRightOutline } from 'flowbite-svelte-icons';
@@ -20,21 +21,29 @@
 
 
 <style>
+
+
 </style>
   
   <main class="container">
-    <h1>Find Your Dream Home</h1>
-    <p>
-      We're working to empower women !!!!
-    </p>
+
+    <div class="mx-auto w-1/2 relative">
+      <h1>Find Your Dream Home</h1>
+      <p>
+        We're working to empower women !!!!
+      </p>
+    
+      <h2>Contact Us</h2>
+      <p>
+        Email: contact@example.com<br />
+        Phone: 123-456-7890
+      </p>
+    </div>
+    <div class="flex items-center justify-center">
+    <Properties/>
+    </div>
+    
   
-    <h2>Contact Us</h2>
-    <p>
-      Email: contact@example.com<br />
-      Phone: 123-456-7890
-    </p>
-
-
 
 
 
