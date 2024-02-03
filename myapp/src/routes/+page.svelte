a10a8186aac51325a9dcf58e468fa676c63ff0f3
7Spook7 <yassinl@vt.edu>
2024-02-03 13:58:26 -0500

    more svelte stuff

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+  /* Import styles from the external CSS file */
+  @import "../styles/LandingPage.css";
+
+  /* Additional component-specific styles can be defined here */
+</style>
+  
+  <main class="container">
+    <h2>About Us</h2>
+    <p>
+      We're working to empower women !!!!
+    </p>
+  
+    <h2>Contact Us</h2>
+    <p>
+      Email: contact@example.com<br />
+      Phone: 123-456-7890
+    </p>
+  </main>
+  
+  
