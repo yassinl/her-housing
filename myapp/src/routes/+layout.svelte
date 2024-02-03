870dabd705f6aacd826037f34ac13a644bde4ba1
7Spook7 <yassinl@vt.edu>
2024-02-03 16:04:26 -0500

    idk anymore man im just the js guy

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -1,15 +1,16 @@
-<Header />
+<Header></Header>
 
 <script>
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import '$lib/styles/style.css';
+	import "../app.pcss";
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+    import '$lib/styles/style.css';
 </script>
 
 <main>
-	<slot />
+	<slot></slot>
 </main>
 
-<Footer />
+<Footer></Footer>
 
 
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+  import { Search, Button } from 'flowbite-svelte';
+  import { MicrophoneSolid, SearchOutline } from 'flowbite-svelte-icons';
+  function handleVoiceBtn() {
+    alert('You clicked voice button');
+  }
+</script>
+
 <style>
 </style>
   
@@ -12,6 +20,32 @@
       Email: contact@example.com<br />
       Phone: 123-456-7890
     </p>
+
+    <form class="flex gap-2">
+      <Search size="md" class="flex gap-2 items-center" placeholder="Search Mockups, Logos, Design Templates...">
+        <button type="button" on:click={handleVoiceBtn} class="outline-none">
+          <MicrophoneSolid class="w-4 h-4 me-2" />
+        </button>
+      </Search>
+      <Button size="sm">
+        <SearchOutline class="w-5 h-5 me-2 -ms-1" />
+        Search
+      </Button>
+    </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   </main>
   
   
