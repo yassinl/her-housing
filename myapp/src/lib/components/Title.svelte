47500a2242ecf30e4f280bd3efeaa704a29546d6
Jerico Manalang <mjerico@vt.edu>
2024-02-03 19:46:06 -0500

    Changed Title

--- myapp/src/lib/components/Title.svelte
+++ myapp/src/lib/components/Title.svelte
@@ -9,7 +9,7 @@
   </script>
   
   <div class="text-center">
-    <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
+    <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Her Housing</Heading>
     <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
     <Button href="/">
       Learn more
